c4f2f61a11e6c3564e0e51a6ae64f99e4b4b9259
Artem <artem256korolev@gmail.com>
2023-09-26 21:27:15 +0300

    changes

---
+++ script.js
@@ -0,0 +1,14 @@
+const menuBtn = document.querySelector('.menu-btn'),
+    menuMobile = document.querySelector('.header-menu-list')
+
+    menuBtn.addEventListener('click', () => {
+        menuMobile.classList.toggle('menu-open')
+    })
+
+    const swiper = new Swiper('.feedback-slider', {
+        loop: true,
+
+        pagination: {
+          el: '.swiper-pagination',
+        }
+      });
