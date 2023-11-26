32e19238835b146df935641ad7140d8fc5ad04f3
Artem <artem256korolev@gmail.com>
2023-11-26 19:56:57 +0300

    initial commit

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const menuBtn = document.querySelector('.menu-btn'),
-    menuMobile = document.querySelector('.header-menu-list')
+  menuMobile = document.querySelector('.header-menu-list')
 
-    menuBtn.addEventListener('click', () => {
-        menuMobile.classList.toggle('menu-open')
-    })
+menuBtn.addEventListener('click', () => {
+    menuMobile.classList.toggle('menu-open')
+})
 
-    const swiper = new Swiper('.feedback-slider', {
-        loop: true,
+const swiper = new Swiper('.feedback-slider', {
+    loop: true,
 
-        pagination: {
-          el: '.swiper-pagination',
-        }
-      });
+    pagination: {
+      el: '.swiper-pagination',
+    }
+  });
