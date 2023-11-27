600996c763c54a0e29dc4f04f48af5445c34d3af
Artem <artem256korolev@gmail.com>
2023-11-27 10:42:04 +0300

    fix burger menu

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const menuBtn = document.querySelector('.menu-btn'),
   menuMobile = document.querySelector('.header-menu-list')
 
 menuBtn.addEventListener('click', () => {
-    menuMobile.classList.toggle('menu-open')
+    menuMobile.classList.toggle('active')
 })
 
 const swiper = new Swiper('.feedback-slider', {
